2fa290c60c7c03ea77bebf9cde34ea23b3a82957
rodrigo-b <rodrigobrito.ciencia@gmail.com>
2024-06-24 22:21:04 -0300

    progress check

--- code/heap.c
+++ code/heap.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 #define arraySize 21
 
 int mainArray [arraySize];
-int lastPosition = 1;
+int lastPosition = 0;
 
 void sift_up (int *array,int index){
 
@@ -31,31 +32,84 @@ void insert(int value){
         return;
     }
 
-    mainArray[lastPosition] = value;
-    sift_up(mainArray, lastPosition);
     lastPosition += 1;
+    mainArray[lastPosition] = value;
+    sift_up(mainArray, lastPosition);    
+}
+
+int get_max(){
+
+    if(lastPosition == 0){
+        printf("array is empty \n");
+        return 0;
+    }
+
+    return mainArray[1];
+}
+
+int get_size(){
+    return lastPosition;
+}
+
+bool is_empty(){
+
+    if(lastPosition == 0){
+        return true;
+    }
+
+    return false;
+}
+
+void sift_down(int *array, int index){
+
+    int firstLeaf = (get_size() / 2) + 1;
+    if(index >= (firstLeaf)){
+        return;
+    }
+
+    int leftChild = index * 2;
+    int rightChild = (index * 2) + 1;
+    int grater  = array[leftChild] > array[rightChild] ? leftChild : rightChild;
+
+    if(array[index] > grater){
+        
+
+    }
+
 }
 
 int main() {
-    
-    //int array [] = {0,10,29,20,18,27,28,17,26,76,16};
-    int array [] = {0,1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
+            
+    printf("isEmpty?  %s", is_empty()? "true \n" : "false \n");
+
+    int array [] = {0,10,29,20,18,27,28,17,26,76,16};
+    //int array [] = {0,1, 3, 5, 4, 6, 13, 10, 9, 8, 15, 17};
     //sift_up(array, 6);
 
    // for(int i = 1; i< 10; i++){
      //   printf("Array position = %d and value = %d \n" ,i, array[i]);
     //}    
 
+    int arraySized = sizeof(array)/sizeof(array[0]);
+    printf("arraySized = %d", arraySized);
 
-    for(int i = 1; i <= 11; i++){
+    for(int i = 1; i < arraySized; i++){
         printf("last position = %d", lastPosition);
         insert(array[i]);
         printf("Array position = %d and value = %d \n" ,i, mainArray[i]);
     }
 
-    for(int i = 1; i<= 11; i++){
+
+
+    for(int i = 1; i< arraySized; i++){
         printf("Array position = %d and value = %d \n" ,i, mainArray[i]);
     }    
+
+    printf("the max value is equal to %d \n", get_max());
+    printf("the number of elements stored is equal to %d \n", get_size());
+    printf("isEmpty?  %s", is_empty()? "true \n" : "false \n");
+
+
 
 
 }
